fb6e08cd99a9d60f5a20536146330333982dfece
David Luis Kim <david8.escola@gmail.com>
2025-08-18 14:53:01 -0300

    Atividade de api

---
+++ 05 - Programação Orientada e Objetos/Exercícios/bancoapi/bin/bancoapi.dart
@@ -0,0 +1,5 @@
+import 'package:bancoapi/bancoapi.dart' as bancoapi;
+
+void main(List<String> arguments) {
+  print('Hello world: ${bancoapi.calculate()}!');
+}
---
+++ 05 - Programação Orientada e Objetos/Exercícios/bancoapi/test/bancoapi_test.dart
@@ -0,0 +1,8 @@
+import 'package:bancoapi/bancoapi.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
