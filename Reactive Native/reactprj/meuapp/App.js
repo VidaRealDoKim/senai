5e7cc2229501dc2b68d7b2ab5ca1996efd613147
David Luis Kim <david8.escola@gmail.com>
2025-08-27 17:32:04 -0300

    new configs

--- Reactive Native/reactprj/meuapp/App.js
+++ Reactive Native/reactprj/meuapp/App.js
@@ -1,11 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -18,3 +16,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
